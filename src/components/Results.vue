7f66cf724b3d38067c47422c4f69f387709b54fc
Neibo Augustine <neiboaugustine@gmail.com>
2022-09-03 16:05:54 +0100

    first commit for reaction timer

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Reaction time : {{score}} ms</p>
+    <p class="rank">{{rank}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Ninja fingers'
+        }else if(this.score < 400){
+            this.rank = ' Rapid reflexes'
+        }else{
+            this.rank = 'Snail Pace'
+        }
+    }
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color:blueviolet;
+    font-weight: bold;
+}
+</style>
